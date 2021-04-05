1c00241adbbdd79e625f9127a976fd466e7a68a7
gromeroc0001 <71333869+gromeroc0001@users.noreply.github.com>
2021-04-04 20:18:48 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,19 @@
+	var var1, var2
+	if(var1==var2){
+		alert("they are the same")
+	}
+		alert("they are the same")
+		alert("they are the same")
+		alert("they are the same")
+		alert("they are the same")
+		alert("they are the same")
+		alert("they are the same")
+		alert("they are the same")
+		alert("they are the same")
+		alert("they are the same")
+		alert("they are the same")
+		alert("they are the same")
+		alert("they are the same")
+		alert("they are the same")
+		alert("they are the same")
+		alert("they are the same")
